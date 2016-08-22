2c2086ec40c3eb41c928af0c4ada8e95a4088e3e
John Dave Decano <johndavedecano@gmail.com>
2016-08-23 01:03:37 +0800

    Added mac menu support

--- src/common/menu/Mac.js
+++ src/common/menu/Mac.js
@@ -1,9 +1,89 @@
-const {Menu} = require('electron')
+const { Menu } = require('electron');
+const { openFile } = require('./events');
 const name = require('electron').remote.app.getName();
 const template = [
   {
+    label: name,
+    submenu: [
+      {
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'hide'
+      },
+      {
+        role: 'hideothers'
+      },
+      {
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  },
+  {
     label: 'File',
-    submenu: []
+    submenu: [
+      {
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+O',
+        click: function(item, focusedWindow) {
+          return openFile(focusedWindow);
+        }
+      },
+      {
+        label: 'Open Folder',
+        click: function() {
+          alert("Open Folder");
+        }
+      },
+      {
+        role: 'quit'
+      }
+    ]
+  },
+  {
+  label: "Edit",
+  submenu: [
+      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+      { type: "separator" },
+      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }]
+  }, {
+    label: 'Tools',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      }
+    ]
   }
 ];
 
